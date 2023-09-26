735064b707c4b68fcdf98309cfb6a96ae35e928e
NatalyaKya <natalya_kya@mail.ru>
2023-09-27 09:41:47 +1000

    fix create card

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -13,7 +13,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((card) => { res.status(201).send({ card }); })
+    .then((card) => { res.status(201).send(card); })
     .catch(next);
 };
 
