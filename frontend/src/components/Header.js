2ab94cc796d76f37cf56d3d9d4a55bcf3ac6c4b9
NatalyaKya <natalya_kya@mail.ru>
2023-09-27 14:50:05 +1000

    fix: nav link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,15 +3,16 @@ import { NavLink, Link } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 function Header(props) {
+  const [isActive, setisActive] = useState(false);
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип проекта Место" />
-      {props.userEmail === "" ?
-        <NavLink className="header__navigation_link" to={props.link}>{props.navigation}</NavLink>
-        : <div className="header__navigation">
+      {props.userEmail ?
+        <div className="header__navigation">
           <span className="header__navigation_email">{props.userEmail}</span>
-          <NavLink to={props.link} className={({ isActive }) => `${isActive ? "header__navigation_link_active" : "header__navigation_link"}`}>{props.navigation}</NavLink>
-        </div>}
+          <NavLink to={props.link} onClick={() => setisActive(!isActive)} className={({ isActive }) => `${isActive ? "header__navigation_link_active" : "header__navigation_link"}`}>{props.navigation}</NavLink>
+        </div>
+        : <NavLink className="header__navigation_link" to={props.link}>{props.navigation}</NavLink>}
     </header>
   );
 }
