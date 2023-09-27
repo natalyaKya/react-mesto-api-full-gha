fb5a2fd6ae048829d36b6c07c911437a1a55c5fd
NatalyaKya <natalya_kya@mail.ru>
2023-09-27 16:35:18 +1000

    fix header logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,17 +6,13 @@ import logo from "../images/logo.svg";
 function Header(props) {
   const [isActive, setisActive] = useState(false);
 
-  function exitAccaunt(props) {
-    setisActive(!isActive);
-    props.signout();
-  }
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип проекта Место" />
       {props.userEmail ?
         <div className="header__navigation">
           <span className="header__navigation_email">{props.userEmail}</span>
-          <span onClick={exitAccaunt} className={({ isActive }) => `${isActive ? "header__navigation_link_active" : "header__navigation_link"}`}>{props.navigation}</span>
+          <span onClick={props.signout} className="header__navigation_link">{props.navigation}</span>
         </div>
         : <NavLink className="header__navigation_link" to={props.link}>{props.navigation}</NavLink>}
     </header>
