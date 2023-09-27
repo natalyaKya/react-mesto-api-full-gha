a6dd38fb63aa350e9d4df24133190c2c670ef114
NatalyaKya <natalya_kya@mail.ru>
2023-09-27 14:11:56 +1000

    fix header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,29 +6,12 @@ function Header(props) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип проекта Место" />
-      {props.userEmail ? (
+      {props.userEmail ?
         <div className="header__navigation">
-          {props.userEmail
-            ? <span className="header__navigation_email">{props.userEmail}</span>
-            : ""
-          }
-          <NavLink
-            to={props.link}
-            className={({ isActive }) =>
-              `${isActive
-                ? "header__navigation_link_active"
-                : "header__navigation_link"
-              }`
-            }
-          >
-            {props.navigation}
-          </NavLink>
+          <span className="header__navigation_email">{props.userEmail}</span>
+          <NavLink to={props.link} className={({ isActive }) => `${isActive ? "header__navigation_link_active" : "header__navigation_link"}`}>{props.navigation}</NavLink>
         </div>
-      ) : (
-        <NavLink className="header__navigation_link" to={props.link}>
-          {props.navigation}
-        </NavLink>
-      )}
+        : <NavLink className="header__navigation_link" to={props.link}>{props.navigation}</NavLink>}
     </header>
   );
 }
